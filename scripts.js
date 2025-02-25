b998b0e8f672020dc937aa5a6f9f05938b125b90
hari199831july <hari199831july@gmail.com>
2025-02-25 10:22:34 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const headers = document.querySelectorAll('.expandable-header');
+    headers.forEach(header => {
+        header.addEventListener('click', function() {
+            headers.forEach(h => {
+                if (h !== header) {
+                    h.classList.remove('expanded-header');
+                    h.nextElementSibling.classList.remove('expanded');
+                    h.textContent = h.textContent.replace('-', '+'); // Change '-' back to '+'
+                }
+            });
+            header.classList.toggle('expanded-header');
+            header.nextElementSibling.classList.toggle('expanded');
+            if (header.classList.contains('expanded-header')) {
+                header.textContent = header.textContent.replace('+', '-'); // Change '+' to '-'
+            } else {
+                header.textContent = header.textContent.replace('-', '+'); // Change '-' back to '+'
+            }
+        });
+    });
+
+    const navLinks = document.querySelectorAll('.navClassHeader nav ul li a');
+    const contents = document.querySelectorAll('.content');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            // Remove 'selected' class from all links
+            navLinks.forEach(navLink => navLink.classList.remove('selected'));
+            
+            // Add 'selected' class to the clicked link
+            this.classList.add('selected');
+
+            // Hide all content sections
+            contents.forEach(content => content.style.display = 'none');
+
+            // Show the target content section
+            const targetId = this.getAttribute('data-target');
+            document.getElementById(targetId).style.display = 'block';
+        });
+    });
+});
